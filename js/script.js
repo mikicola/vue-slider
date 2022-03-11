8aadc528d7302b6dc637355e690cda1d0580322c
mikicola <michela.colangelo@gmail.com>
2022-03-11 17:50:45 +0100

    funzione bottoni

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -21,6 +22,7 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+*/
 
 // const app = ({
 //     el: '#root',
@@ -31,10 +33,17 @@ const text = [
 
 // const root = new Vue(app);
 
+// Descrizione:
+// Partendo dai file nello starter implementare la funzione dei bottoni (next e previous) dello slider usando Vue (prima però create la struttura dati).
+// Bonus:
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 
 const app = new Vue({
     el: '#root',
     data: {
+        activeIndexImg: 0,
         arrSlides:[
             {
                 item: '01.jpg',
@@ -62,5 +71,21 @@ const app = new Vue({
                 text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
             }
         ]
+    },
+    methods:{
+        btnNext(){
+            if(this.activeIndexImg == this.arrSlides.length - 1){
+                this.activeIndexImg = 0;
+            }else{
+                this.activeIndexImg++;
+            }
+        },
+        btnPrevius(){
+            if(this.activeIndexImg == 0){
+            this.activeIndexImg = this.arrSlides.length -1;
+            }else{
+                this.activeIndexImg--;
+            }
+        },
     }
 })
